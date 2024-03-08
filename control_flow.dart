c6f87e555c852616fba3ffca7c12cb5f8554570d
linder <achielinder@gmail.com>
2024-03-08 14:28:28 +0300

    done

---
+++ control_flow.dart
@@ -0,0 +1,12 @@
+void main(){
+   int x = 20;
+   if (x > 10) {
+   print ("Your number is greater than 10");
+   }
+   else if (x < 10) {
+   print ("Your number is less than 10");
+   }
+   else {
+    print ( "Your number is equal to 10");
+   }
+}
